22ac9a21461a889818e8d03f882762c503af3ec2
Vibhor Malik <vibh1103@gmail.com>
2021-01-10 20:08:22 +0530

    Navigation bar code

---
+++ public/js/style.js
@@ -0,0 +1,21 @@
+$(".modal").draggable({
+    handle: ".modal-header"
+  });
+
+
+// modal 5
+
+var $wrap = $('#main');
+        var $signUpBtn = $wrap.find('#signUpBtn');
+        var $loginBtn = $wrap.find("#loginBtn");
+        
+        $signUpBtn.on('click', function() {
+            $wrap.addClass('singUpActive');
+            $wrap.removeClass('loginActive');
+        });
+        
+        $loginBtn.on('click', function() {
+            $wrap.addClass('loginActive');
+            $wrap.removeClass('singUpActive');
+        });
+
