8951dcd422bd6d8af3c53d8440948bb364ec2ec2
Heena Sharma <heena.sharma@thoughts2binary.com>
2021-01-29 23:26:55 +0530

    Validations

--- src/shared/components/basket_card.js
+++ src/shared/components/basket_card.js
@@ -9,76 +9,111 @@ export default class BasketCard extends Component {
         this.state = {
             quantity: {},
             clarifications: "",
-            selected: 0
+            selected: 0,
+            iserror: false,
+            showError: false,
+            errors: {
+                quantity: ""
+            }
         }
         this.common = new Common();
         this.CartOperation = new CartOperation()
     }
-    add_to_cart = data => async(e) => {
-        var cartData = {
-          data: data,
-          quantity: this.state.quantity,
-          clarifications: this.state.clarifications
+    validate = async () => {
+        this.setState({ iserror: false })
+        let errors = this.state.errors
+        let quantity = 0
+        for (const value in this.state.quantity){
+            quantity = quantity + this.state.quantity[value]
         }
-        await this.CartOperation.setNewCartElement(cartData)
-        this.removeModal()
-      }
+        if (quantity == 0) {
+            this.setState({ iserror: true })
+            errors.quantity = "Please select quantity"
+        }
+        else {
+            errors.quantity = ""
+        }
+    }
+    add_to_cart = data => async (e) => {
+        var cartData = {
+            data: data,
+            quantity: this.state.quantity,
+            clarifications: this.state.clarifications
+        }
+        await this.validate()
+        if (this.state.iserror) {
+            this.setState({ showError: true })
+        }
+        else {
+            await this.CartOperation.setNewCartElement(cartData)
+            this.removeModal()
+        }
 
-    removeModal = async() => {
-        this.setState({quantity: {}, clarifications: "", selected: 0})
+    }
+
+    removeModal = async () => {
+        this.setState({
+            quantity: {}, clarifications: "", selected: 0, iserror: false,
+            showError: false,
+            errors: {
+                quantity: ""
+            }
+        })
     }
     handleState = (value, element) => {
+        this.setState({ showError: false })
         this.state.quantity[element] = value
-        this.setState({selected: value})
-      }
+        this.setState({ selected: value })
+    }
     handleChange = (event) => {
-    this.setState({
-        [event.target.name]: event.target.value
-    });
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     }
     render() {
-        const {element} = this.props;
+        const { element } = this.props;
         return (
-                
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-lg-9 col-md-9 col-sm-9">
-                                    <h5 className="card-title"> <strong style={{ "white-space": "pre-wrap" }}>{element.name}</strong></h5>
-                                </div>
-                                <div className="col-lg-3 col-md-3 col-sm-3">
-                                    <h5 className={element.type === "empanadas" ? "empanadas-precio" : "canastitas-precio"}>{element.price}</h5>
-                                </div>
-                            </div>
-                            <div className="row">
-                                {
-                                    this.common.slice_object(element.options, 4).map(ele => {
-                                        return (
-                                            <div className="col-lg-6 col-md-6 col-sm-6">
-                                                {ele.map(element => (
-                                                    <>
-                                                        <Dropdown limit={13} quantity={this.handleState} name={element} selected={this.state.selected} />
-                                                        <span className="caret">{element}</span> <br />
-                                                    </>
-                                                ))}
-                                            </div>
 
-                                        )
-                                    })
-                                }
-                                {
-                                    element.image &&
-                                    <div class="col-lg-6 col-md-6 col-sm-6">
-                                        <img src={element.image} alt={element.type === "empanadas" ? "empanada de carne" : "canastitas especiales"} class={element.type === "empanadas" ? "empanadas-img" : "canastitas-img"} />
-                                    </div>
-
-                                }
-                            </div>
-                            <input type="text" placeholder="Aclaraciones" className="aclaraciones" name={"clarifications"} value={this.state.clarifications} onChange={this.handleChange}></input>
-                            <button className="btn btn-danger  btn-block" value="AGREGAR A MI PEDIDO" onClick={this.add_to_cart(element)}>AGREGAR A MI PEDIDO</button>
+            <div className="card">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-lg-9 col-md-9 col-sm-9">
+                            <h5 className="card-title"> <strong style={{ "white-space": "pre-wrap" }}>{element.name}</strong></h5>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            <h5 className={element.type === "empanadas" ? "empanadas-precio" : "canastitas-precio"}>{element.price}</h5>
                         </div>
                     </div>
-                
+                    <div className="row">
+                        {
+                            this.common.slice_object(element.options, 4).map(ele => {
+                                return (
+                                    <div className="col-lg-6 col-md-6 col-sm-6">
+                                        {ele.map(element => (
+                                            <>
+                                                <Dropdown limit={13} quantity={this.handleState} name={element} selected={this.state.selected} />
+                                                <span className="caret">{element}</span> <br />
+                                            </>
+                                        ))}
+                                    </div>
+
+                                )
+                            })
+                        }
+                        {
+                            element.image &&
+                            <div class="col-lg-6 col-md-6 col-sm-6">
+                                <img src={element.image} alt={element.type === "empanadas" ? "empanada de carne" : "canastitas especiales"} class={element.type === "empanadas" ? "empanadas-img" : "canastitas-img"} />
+                            </div>
+
+                        }
+                    </div>
+                    <input type="text" placeholder="Aclaraciones" className="aclaraciones" name={"clarifications"} value={this.state.clarifications} onChange={this.handleChange}></input>
+                    <label style={{ color: 'red' }}>{this.state.showError && this.state.errors.quantity}</label>
+                    <button className="btn btn-danger  btn-block" value="AGREGAR A MI PEDIDO" onClick={this.add_to_cart(element)}>AGREGAR A MI PEDIDO</button>
+                </div>
+            </div>
+
         )
     }
 }
--- src/shared/components/more_promotion_modal.js
+++ src/shared/components/more_promotion_modal.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Dropdown from "./dropdown";
 import CartOperation from "../../core/utility/cart";
+import Common from "../../core/utility/common";
 
 class MorePromotionModal extends Component {
   constructor(props) {
     super(props)
     this.cart = new CartOperation();
+    this.common = new Common();
   }
   modelStyle = {
     width: '100%',
@@ -19,7 +21,12 @@ class MorePromotionModal extends Component {
       canastitas: {},
       total_quantity: 0
     },
-    clarifications: ""
+    clarifications: "",
+    iserror: false,
+    showError: false,
+    errors: {
+      quantity: ""
+    }
   }
   removeModal = () => {
     this.props.closeModal()
@@ -29,27 +36,63 @@ class MorePromotionModal extends Component {
       total_quantity: 0
     }
     this.setState({
-    clarifications: ""
-  })
+      clarifications: "",
+      iserror: false,
+      showError: false,
+      errors: {
+        quantity: ""
+      }
+    })
   }
-  add_to_cart = async() => {
+  validate = async () => {
+    this.setState({ iserror: false })
+    let errors = this.state.errors
+    console.log(this.state.quantity)
+    let quantity = 0
+    for (const value in this.state.quantity.empanadas) {
+      quantity = quantity + this.state.quantity.empanadas[value]
+    }
+    for (const value in this.state.quantity.canastitas) {
+      quantity = quantity + this.state.quantity.canastitas[value]
+    }
+    if (quantity == 0) {
+      this.setState({ iserror: true })
+      errors.quantity = "Please select atleast one element"
+    }
+    else if (this.state.quantity.total_quantity == 0) {
+      this.setState({ iserror: true })
+      errors.quantity = "Please select total quantity"
+    }
+    else {
+      errors.quantity = ""
+    }
+  }
+  add_to_cart = async () => {
     var cartData = {
       data: this.props.selectedData,
       quantity: this.state.quantity,
       clarifications: this.state.clarifications
     }
-    await this.cart.setNewCartElement(cartData)
-    this.removeModal()
+    await this.validate()
+    if (this.state.iserror) {
+      this.setState({ showError: true })
+    }
+    else {
+      await this.cart.setNewCartElement(cartData)
+      this.removeModal()
+    }
   }
   handleState = (value, element) => {
-    if (element === "quantity"){
-      this.state.quantity.total_quantity = value; 
+    this.setState({ showError: false })
+    if (element === "quantity") {
+      this.state.quantity.total_quantity = value;
     }
     else {
       this.state.quantity[element.parent][element.name] = value
     }
   }
   handleChange = (event) => {
+    this.setState({ showError: false })
     this.setState({
       [event.target.name]: event.target.value
     });
@@ -59,85 +102,86 @@ class MorePromotionModal extends Component {
     return (
       <React.Fragment>
         {visible ? (
-      <section className="modal1">
-        <div className="modal fade" data-backdrop="false" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" >
-          <div className="modal-dialog modal-lg" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h4 className="modal-title" id="myModalLabel">¡Elegi tus empanadas o canastitas para la promo!</h4>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.removeModal}><span aria-hidden="true">&times;</span></button>
-              </div>
-              <div className="modal-body">
-                <section className="empanadas">
+          <section className="modal1">
+            <div className="modal fade" data-backdrop="false" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" >
+              <div className="modal-dialog modal-lg" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h4 className="modal-title" id="myModalLabel">¡Elegi tus empanadas o canastitas para la promo!</h4>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.removeModal}><span aria-hidden="true">&times;</span></button>
+                  </div>
+                  <div className="modal-body">
+                    <section className="empanadas">
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-6">
+                            <div className="card">
+                              <div className="card-body">
+                                <div className="row">
+                                  <div className="col">
+                                    <h5 className="card-title"><strong>EMPANADAS</strong></h5>
+                                  </div>
+                                </div>
+                                <div className="row">
+                                  <div className="col">
+                                    {this.state.empanadas.map(element => (
+                                      <>
+                                        <Dropdown limit={13} quantity={this.handleState} name={{ "parent": "empanadas", "name": element }} />
+                                        <span className="caret">{element}</span> <br />
+                                      </>
+                                    ))}
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-6">
+                            <div className="card">
+                              <div className="card-body">
+                                <div className="row">
+                                  <div className="col">
+                                    <h5 className="card-title"><strong>CANASTITAS</strong></h5>
+                                  </div>
+                                </div>
+                                <div className="row">
+                                  <div className="col">
+                                    {this.state.canastitas.map(element => (
+                                      <>
+                                        <Dropdown limit={13} quantity={this.handleState} name={{ "parent": "canastitas", "name": element }} />
+                                        <span className="caret">{element}</span> <br />
+                                      </>
+                                    ))}
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <input type="text" placeholder="Aclaraciones" name={"clarifications"} style={this.modelStyle} onChange={this.handleChange}></input>
+                        </div>
+                      </div>
+                    </section>
+                  </div>
                   <div className="container">
-                    <div className="row">
-                      <div className="col-6">
-                        <div className="card">
-                          <div className="card-body">
-                            <div className="row">
-                              <div className="col">
-                                <h5 className="card-title"><strong>EMPANADAS</strong></h5>
-                              </div>
-                            </div>
-                            <div className="row">
-                              <div className="col">
-                                {this.state.empanadas.map(element => (
-                                  <>
-                                    <Dropdown limit={13} quantity={this.handleState} name={{"parent": "empanadas", "name": element}}/>
-                                    <span className="caret">{element}</span> <br />
-                                  </>
-                                ))}
-                              </div>
-                            </div>
-                          </div>
+                    <div className="footer">
+                      <div className="row">
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                          <Dropdown limit={5} quantity={this.handleState} />
+                          <span className="caret">  Cantidad</span>
                         </div>
-                      </div>
-                      <div className="col-6">
-                        <div className="card">
-                          <div className="card-body">
-                            <div className="row">
-                              <div className="col">
-                                <h5 className="card-title"><strong>CANASTITAS</strong></h5>
-                              </div>
-                            </div>
-                            <div className="row">
-                              <div className="col">
-                                {this.state.canastitas.map(element => (
-                                  <>
-                                    <Dropdown limit={13} quantity={this.handleState} name={{"parent": "canastitas", "name": element}}/>
-                                    <span className="caret">{element}</span> <br />
-                                  </>
-                                ))}
-                              </div>
-                            </div>
-                          </div>
+                        <div className="col-lg-9 col-md-9 col-sm-9">
+                          <label style={{ color: 'red' }}>{this.state.showError && this.state.errors.quantity}</label>
+                          <button className="btn btn-danger  btn-block" value="AGREGAR A MI PEDIDO" onClick={this.add_to_cart}>AGREGAR A MI PEDIDO</button>
                         </div>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <input type="text" placeholder="Aclaraciones" name={"clarifications"} style={this.modelStyle} onChange={this.handleChange}></input>
+                      </div> <br />
                     </div>
                   </div>
-                </section>
-              </div>
-              <div className="container">
-                <div className="footer">
-                  <div className="row">
-                    <div className="col-lg-3 col-md-3 col-sm-3">
-                      <Dropdown limit={5} quantity={this.handleState}/>
-                      <span className="caret">  Cantidad</span>
-                    </div>
-                    <div className="col-lg-9 col-md-9 col-sm-9">
-                      <button className="btn btn-danger  btn-block" value="AGREGAR A MI PEDIDO" data-dismiss="modal" onClick={this.add_to_cart}>AGREGAR A MI PEDIDO</button>
-                    </div>
-                  </div> <br />
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-      ) : null}
+          </section>
+        ) : null}
       </React.Fragment>
     );
   }
